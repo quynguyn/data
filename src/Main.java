d564b31a400b03d80866fd7173db6406245127ec
quynguyn <86903423+quynguyn@users.noreply.github.com>
2021-12-17 23:25:08 +0700

    save the map on 2D array, find the biggest gold value in map, print out maze function

--- src/Main.java
+++ src/Main.java
@@ -17,12 +17,54 @@ public class Main {
         System.out.println(row+ " "+ col);
 
         // taking every char in the map
+        char [][] maze = new char[row][col];
+        int count_row = 0, count_col = 0, highest_gold = 0;
         while (scan.hasNextLine()){
             String data =  scan.nextLine();
             for (char c: data.toCharArray())
             {
-                System.out.print(c + "/");
+                if(c == ' '){
+                    continue;
+                }
+                //find the biggest value
+                if(c != 'X' && c != '.') {
+                    int check  = Integer.parseInt(String.valueOf(c));
+                    if(check >= highest_gold)
+                    highest_gold = check;
+                }
+                maze[count_row][count_col] = c;
+//                System.out.print(maze[count_row][count_col] + "|"+  count_row + "|" + count_col +"\n");
+                count_col++;
+                if (count_col == col){
+                    count_row ++;
+                    count_col = 0;
+                }
             }
         }
+
+        System.out.println(highest_gold);
+        print_maze(maze,row,col);
+    }
+    //move left
+    //move down
+    //pick up gold
+    //mark path
+    //gold count
+    //steps count
+    
+
+    public static void print_maze(char[][] maze, int row, int col) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(maze[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+
+
+    public static char pick_up_gold(int row, int col){
+        return 'G';
     }
 }
+
