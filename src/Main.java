3b048893f22cf3fe8c050b5285d29ea77ac8772f
quynguyn <86903423+quynguyn@users.noreply.github.com>
2022-01-09 14:46:48 +0700

    9/1/2021 working on dynamic - noon

--- src/Main.java
+++ src/Main.java
@@ -41,27 +41,42 @@ public class Main {
         System.out.println(row+ " "+ col);
 
         // taking every char in the map
-        char[][] maze = new char[row][col];
-
-        while (scan.hasNextLine()) {
-            String data = scan.nextLine();
-            for (char c : data.toCharArray()) {
-                if(c == ' ') continue;
-                maze[count_row][count_col] = c;
-                count_col++;
-                if (count_col == col) {
-                    count_row++;
-                    count_col = 0;
-                }
-                if(count_row == row) break;
+        String[][] maze = new String[row][col];
+        //skip last \n of first line
+        scan.nextLine();
+        while(scan.hasNextLine()){
+            String line = scan.nextLine();
+            String [] data = line.split(" ");
+            for(String char_data: data)
+            {
+                maze[count_row][count_col] = char_data;
+                if(count_col < col-1) count_col++;
             }
+            count_col = 0;
+            count_row++;
+            if(count_row == row) break;
         }
+
+//        while (scan.hasNextLine()) {
+//            String data = scan.nextLine();
+//            for (char c : data.toCharArray()) {
+//                if(c == ' ') continue;
+//                maze[count_row][count_col] = c;
+//                count_col++;
+//                if (count_col == col) {
+//                    count_row++;
+//                    count_col = 0;
+//                }
+//                if(count_row == row) break;
+//            }
+//        }
 
 
         print_maze(maze, row, col);
-
+//
         // run code
         long start = System.nanoTime();
+        exhaustive_search(maze, pos_x, pos_y, count, row, col, step, direction, path);
         dynamic_search(maze, pos_x, pos_y, count, row, col, step, direction, path);
         long finish = System.nanoTime();
         long timeElapsed = finish - start;
@@ -75,21 +90,29 @@ public class Main {
 //        System.out.println(memo);
 
     }
+    public static boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
+    }
 
-    public static void change_maze(char[][] maze) {
+    public static void change_maze(String[][] maze) {
         int pathRow = 0;
         int pathCol = 0;
-        maze[pathRow][pathCol] = '+';
+        maze[pathRow][pathCol] = "+";
         for (int i = 0; i < realPath.length(); i++) {
             if (realPath.charAt(i) == 'D') {
                 pathRow++;
             } else if (realPath.charAt(i) == 'R') {
                 pathCol++;
             }
-            if (maze[pathRow][pathCol] == '.') {
-                maze[pathRow][pathCol] = '+';
+            if (maze[pathRow][pathCol].equals(".")) {
+                maze[pathRow][pathCol] = "+";
             } else {
-                maze[pathRow][pathCol] = 'G';
+                maze[pathRow][pathCol] = "G";
             }
         }
     }
@@ -105,7 +128,7 @@ public class Main {
         }
     }
 
-    public static void dynamic_search(char[][] maze, int pos_x, int pos_y, int count, int row, int col, int step,
+    public static void dynamic_search(String[][] maze, int pos_x, int pos_y, int count, int row, int col, int step,
                                       String direction, String path) {
 
         int maxGold = 0;
@@ -114,46 +137,52 @@ public class Main {
         int up = 0;
         int left = 0;
         int temp = 0;
-        char[][] newMaze = new char[row][col];
+        String[][] newMaze = new String[row][col];
         for(int x = 0; x < row; x ++)
         {
             for(int y = 0; y < col; y++)
             {
                 if(x== 0 && y== 0){
-                    newMaze[x][y] = '0';
+                    newMaze[x][y] = "0";
                     continue;
                 }
-                else if(maze[x][y]=='X'){
-                    newMaze[x][y] ='X';
+                else if(maze[x][y].equals("X")){
+                    newMaze[x][y] ="X";
                     //temp = Integer.parseInt(String.valueOf(maze[x][y]));
                     continue;
                 }
-                else if(maze[x][y] =='.')
+                else if(maze[x][y].equals("."))
                 {
-                        newMaze[x][y] = (char)('0'+temp);
+                        Integer value = temp;
+                        newMaze[x][y] = value.toString();
 
                 }
-                if(maze[x][y]!='.' && x == 0){
-                    temp = Integer.parseInt(String.valueOf(maze[x][y])) + Integer.parseInt(String.valueOf(newMaze[x][y-1]));
-
-                    newMaze[x][y] = (char)('0'+temp);
+                if(!maze[x][y].equals(".") && x == 0){
+                    temp = Integer.parseInt(maze[x][y]) + Integer.parseInt(newMaze[x][y-1]);
+                    int value = temp;
+                    newMaze[x][y] = Integer.toString(value);
+//                    newMaze[x][y] = (char)('0'+temp);
 //                    temp = 0;
 //                    System.out.println(newMaze[x][y]);
                 }
-                else if(maze[x][y] != '.' && y ==0){
+                else if(!maze[x][y].equals(".") && y ==0){
                     temp = Integer.parseInt(String.valueOf(maze[x][y])) + Integer.parseInt(String.valueOf(newMaze[x-1][y]));
-                    newMaze[x][y] = (char)('0'+temp);
+                    int value = temp;
+                    newMaze[x][y] = Integer.toString(value);
+//                    newMaze[x][y] = (char)('0'+temp);
 //                    temp = 0;
                 }
-                else if(maze[x][y]!='.'){
-                    if(maze[x-1][y]!='X'){
+                else if(!maze[x][y].equals(".")){
+                    if(!maze[x - 1][y].equals("X")){
                         up = Integer.parseInt(String.valueOf(newMaze[x-1][y]));
                     }
-                    if(maze[x][y-1]!='X'){
+                    if(!maze[x][y - 1].equals("X")){
                         left = Integer.parseInt(String.valueOf(newMaze[x][y-1]));
                     }
                     temp = up + left + Integer.parseInt(String.valueOf(maze[x][y]));
-                    newMaze[x][y] = (char)('0'+temp);
+                    int value = temp;
+                    newMaze[x][y] = Integer.toString(value);
+//                    newMaze[x][y] = (char)('0'+temp);
 //                    temp = 0;
                 }
                 if(y==col-1) temp = 0;
@@ -190,18 +219,18 @@ public class Main {
 //        time++;
 //    }
     }
-    public static void exhaustive_search(char[][] maze, int pos_x, int pos_y, int count, int row, int col, int step,
+    public static void exhaustive_search(String[][] maze, int pos_x, int pos_y, int count, int row, int col, int step,
                                          String direction, String path) {
 
         //end of recursion
         if (pos_x == row || pos_y == col) {
             return;
         }
-        if (maze[pos_x][pos_y] == 'X') {
+        if (maze[pos_x][pos_y].equals("X")) {
             return;
         }
 
-        if (maze[pos_x][pos_y] != '.') {
+        if (!maze[pos_x][pos_y].equals(".")) {
             count += Integer.parseInt(String.valueOf(maze[pos_x][pos_y]));
         }
         path += direction;
@@ -212,7 +241,7 @@ public class Main {
         time++;
     }
 
-    public static void print_maze(char[][] maze, int row, int col) {
+    public static void print_maze(String[][] maze, int row, int col) {
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 System.out.print(maze[i][j] + " ");
